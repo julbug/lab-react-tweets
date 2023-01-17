9899cbf2a8fe643c58252e10ca98cf454ef5fe95
Julia Dannerth <jdannerth@gmail.com>
2023-01-17 17:07:50 -0500

    dogs changed to cats

--- src/App.js
+++ src/App.js
@@ -5,33 +5,33 @@ import Tweet from "./components/Tweet";
 const tweetsArray = [
   {
     user: {
-      name: "Thoughts of Dog®",
-      image: "https://i.imgur.com/b0EdHVV.jpg",
-      handle: "dog_feelings",
+      name: "Thoughts of Cat®",
+      image: "https://media.istockphoto.com/id/1322123064/photo/portrait-of-an-adorable-white-cat-in-sunglasses-and-an-shirt-lies-on-a-fabric-hammock.jpg?s=612x612&w=0&k=20&c=-G6l2c4jNI0y4cenh-t3qxvIQzVCOqOYZNvrRA7ZU5o=",
+      handle: "cat_feelings",
     },
     timestamp: "1h ago",
     message:
-      "the human likes to say. that i live here rent free. but i would argue. this housing accommodation. is my payment. for a lifetime of love. and excellent company",
+      "Relaxing now so I can run around for no reason at 3 A.M.",
   },
   {
     user: {
-      name: "Thoughts of Dog®",
-      image: "https://i.imgur.com/b0EdHVV.jpg",
-      handle: "dog_feelings",
+      name: "Thoughts of Cat®",
+      image: "https://media.istockphoto.com/id/1322123064/photo/portrait-of-an-adorable-white-cat-in-sunglasses-and-an-shirt-lies-on-a-fabric-hammock.jpg?s=612x612&w=0&k=20&c=-G6l2c4jNI0y4cenh-t3qxvIQzVCOqOYZNvrRA7ZU5o=",
+      handle: "cat_feelings",
     },
     timestamp: "2h ago",
     message:
-      "sometimes. the human presses their noggin against mine. to figure out what i’m thinking. so i just think really hard. about how much i love them. and hope they figure it out",
+      "Meowed until the human woke up. I didn't need anything, I was just bored",
   },
   {
     user: {
-      name: "Thoughts of Dog®",
-      image: "https://i.imgur.com/b0EdHVV.jpg",
-      handle: "dog_feelings",
+      name: "Thoughts of Cat®",
+      image: "https://media.istockphoto.com/id/1322123064/photo/portrait-of-an-adorable-white-cat-in-sunglasses-and-an-shirt-lies-on-a-fabric-hammock.jpg?s=612x612&w=0&k=20&c=-G6l2c4jNI0y4cenh-t3qxvIQzVCOqOYZNvrRA7ZU5o=",
+      handle: "cat_feelings",
     },
     timestamp: "3h ago",
     message:
-      "here is what. i plan to accomplish today: \n\n2. bark loudly. but at nothing \n7. lose my ball under the couch\n7b. politely ask the human. to get my ball\n3. immediately lose it again. under the same couch\n4. big nap. you have worked hard\n2. repeat",
+      "here is what. i plan to accomplish today: \n\n1. sleep \n2. cough up a hairball \n2b. stare at human until they clean up the hairball\n3. eat too fast \n4. puke on the carpet after eating\n5. cat nap",
   },
 ];
 
